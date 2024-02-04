c788ebbc317a188b63d49fc1792442815957bb35
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-02-04 17:28:02 +0100

    add API fetch & exchange rate display

--- App.tsx
+++ App.tsx
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import getRate from './api';
+
 
 export default function App() {
+
+  const [rate, setRate] = useState<number>(0)
+  const [baseCode, setBaseCode] = useState<string>('')
+  const [targetCode, setTargetCode] = useState<string>('')
+
+  // const [fetchedRate, setFetchedRate] = useState<number>(0)
+
+
+  const handlePress = async (baseCode: string, targetCode: string) => {
+    try {
+      const fetchedRate: number = await getRate(baseCode, targetCode);
+      setRate(fetchedRate);
+
+      setTargetCode(targetCode)
+      setBaseCode(baseCode)
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <View style={{}}>
+        <Text>Currency calculator</Text>
+      </View>
+
+      {/* <Pressable onPress={() => getRate('EUR', 'PLN')}> */}
+      <Pressable onPress={() => handlePress("EUR", "PLN")}>
+        <Text>getRates</Text>
+      </Pressable>
+      {!!rate && <Text>1 {baseCode} = {rate} {targetCode}</Text>}
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -13,8 +47,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ api.ts
@@ -0,0 +1,29 @@
+// currency1 = 'EUR'
+// currency2 = 'PLN'
+
+const getRate = async (baseCode: string, targetCode: string) => {
+  const apiUrl: string = `https://v6.exchangerate-api.com/v6/${process.env.EXPO_PUBLIC_API_KEY}/pair/${baseCode}/${targetCode}`;
+
+  const response = await fetch(apiUrl);
+  const json = await response.json();
+  const rate: number = json.conversion_rate;
+
+  console.log(json);
+  console.log(rate);
+  return rate;
+};
+
+// JSON response
+
+// base_code: "EUR"
+// conversion_rate: 4.316
+// documentation: "https://www.exchangerate-api.com/docs"
+// result: "success"
+// target_code: "PLN"
+// terms_of_use: "https://www.exchangerate-api.com/terms"
+// time_last_update_unix: 1707004801
+// time_last_update_utc: "Sun, 04 Feb 2024 00:00:01 +0000"
+// time_next_update_unix: 1707091201
+// time_next_update_utc: "Mon, 05 Feb 2024 00:00:01 +0000"
+
+export default getRate;
