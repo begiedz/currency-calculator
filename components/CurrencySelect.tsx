f151ba04fdd6213a407bfecbc872d0e33aecd394
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-05-07 13:59:33 +0200

    add RN Elements SeatchBar

--- components/CurrencySelect.tsx
+++ components/CurrencySelect.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import { SafeAreaView, Text, TextInput, View, FlatList, Pressable, Animated, TouchableHighlight } from 'react-native'
+import { SafeAreaView, Text, TextInput, View, FlatList, Pressable, Animated, TouchableHighlight, Platform } from 'react-native'
 
 import { useAppContext } from '../Context'
 import { supportedCurrencies } from '../data/supportedCurrencies'
 import { Styles } from '../styles/Styles'
 import { useNavigation } from '@react-navigation/native'
+import { Icon, SearchBar } from '@rneui/base'
 
 const CurrencySelect = ({ route }: any) => {
   const navigation = useNavigation();
@@ -30,45 +31,48 @@ const CurrencySelect = ({ route }: any) => {
           //@ts-ignore
           navigation.navigate('Calculator');
         }}>
-
         <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '95%' }}>
+
           <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', }}>
             <View>
               <Text style={{ color: 'white', fontSize: 36, marginRight: 10 }}>{flag}</Text>
             </View>
             <View>
-              <Text style={{ color: 'white', fontSize: 16 }}>{name}</Text>
+              <Text style={{ color: 'white', fontSize: 18 }}>{name}</Text>
             </View>
           </View>
           <View>
             <Text style={{ color: 'gray', marginLeft: 'auto', fontWeight: '700' }}>{code}</Text>
           </View>
+
         </View>
       </TouchableHighlight >
     )
   }
-
   return (
     <SafeAreaView style={Styles.backGround}>
-      <TextInput
-        style={{ width: '90%', backgroundColor: '#222', borderRadius: 10, padding: 10, marginTop: 16, fontSize: 18, color: 'white' }}
-        placeholder='Search'
-        placeholderTextColor="gray"
-      // onChangeText={setSearchCode}
-      // value={searchCode} 
-      />
-      <View style={{ width: '100%' }}>
-        <FlatList
-          showsVerticalScrollIndicator={true}
-          style={{ marginTop: 16 }}
-          data={supportedCurrencies}
-          renderItem={({ item }) => (
-            <ListElement code={item.code} name={item.name} flag={item.flag} />)}
-          ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
-        />
-      </View>
-    </SafeAreaView >
 
+      <SearchBar
+        containerStyle={{ backgroundColor: 'transparent', width: '100%' }}
+        inputContainerStyle={{ backgroundColor: '#222', }}
+
+        onChangeText={(event) => { console.log(event); }}
+        placeholder={`Search ${changeTargetCode ? 'target' : 'base'} currency`}
+        platform={Platform.OS === 'ios' || "deafult" ? 'ios' : 'android'}
+        searchIcon={Platform.OS === 'ios' || "deafult" ? { name: 'search' } : { name: 'magnify', type: 'material-community' }}
+        clearIcon={Platform.OS === 'ios' || "deafult" ? { name: 'close-circle' } : { name: 'close-circle', type: 'material-community' }}
+      />
+
+      <FlatList
+        showsVerticalScrollIndicator={true}
+        style={{ marginTop: 16, width: '100%' }}
+        data={supportedCurrencies}
+        renderItem={({ item }) => (
+          <ListElement code={item.code} name={item.name} flag={item.flag} />)}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+      />
+
+    </SafeAreaView >
   )
 }
 
